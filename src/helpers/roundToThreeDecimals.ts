c7a32dd4029beb380867bfc493393d21d809096b
Patrik Setterberg <setterberg.patrik@gmail.com>
2024-04-28 18:30:46 +0200

    Add typehints in roundToThreeDecimals doc comment.

--- src/helpers/roundToThreeDecimals.ts
+++ src/helpers/roundToThreeDecimals.ts
@@ -1,7 +1,7 @@
 /**
  * Rounds a number to three decimal places.
- * @param num - The number to be rounded.
- * @returns The rounded number.
+ * @param {number} num - The number to be rounded.
+ * @returns {number} The rounded number.
  */
 const roundToThreeDecimals = (num: number) => {
   return Math.round((num + Number.EPSILON) * 1000) / 1000;
