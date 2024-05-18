465a0d8cc951b1a5f7c4b4bfc016f85a1649c619
Patrik Setterberg <setterberg.patrik@gmail.com>
2024-05-18 10:16:27 +0200

    Add Link component.

---
+++ src/components/Link.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from "react";
+import clsx from "clsx/lite";
+
+type LinkProps = {
+    href: string;
+    children: ReactNode;
+    newTab?: boolean;
+};
+
+/**
+ * A custom link component.
+ *
+ * @component
+ * @param {object} props - The component props.
+ * @param {string} props.href - The URL of the link.
+ * @param {ReactNode} props.children - The content of the link.
+ * @param {boolean} props.newTab - Whether to open the link in a new tab.
+ * @returns {JSX.Element} The rendered link component.
+ */
+const Link = ({ href, children, newTab }: LinkProps): JSX.Element => {
+    const linkProps = newTab
+        ? { target: "_blank", rel: "noopener noreferrer" }
+        : {};
+
+    return (
+        <a
+            href={href}
+            className={clsx(
+                "rounded-sm font-semibold text-blue underline decoration-transparent decoration-2 underline-offset-[3px] outline outline-2 outline-offset-2 outline-transparent",
+                "hover:text-light-blue hover:decoration-current hover:decoration-2",
+                "focus-visible:text-light-blue focus-visible:outline-current",
+                "transition-colors duration-100 ease-out",
+            )}
+            {...linkProps}
+        >
+            {children}
+        </a>
+    );
+};
+
+export default Link;
